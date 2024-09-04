c3bcc4a987fa514e2ab765d90d68abd1e8873771
Mai2aa <mysaadil0@gmail.com>
2024-09-04 04:47:05 +0300

    advanced

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,8 +14,13 @@ class Auth:
             return True
         normalized_path = path.rstrip('/')
         for excluded in excluded_paths:
-            if normalized_path == excluded.rstrip('/'):
-                return False
+            if excluded.endswith('*'):
+                base_path = excluded[:-1].rstrip('/')
+                if normalized_path.startswith(base_path):
+                    return False
+            else:
+                if normalized_path == excluded.rstrip('/'):
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
