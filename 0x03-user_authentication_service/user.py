7ac4dfb7142734c03f238838de2a905407c9d9ac
Mai2aa <mysaadil0@gmail.com>
2024-09-10 23:08:44 +0300

    user model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,4 +1,4 @@
-#!/usr/bin/ env python3
+#!/usr/bin/env python3
 ''' User model '''
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
