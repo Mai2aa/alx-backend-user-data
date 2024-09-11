106e9be0acd80241cf8b3de7d8e9b7b05fc346a3
Mai2aa <mysaadil0@gmail.com>
2024-09-11 18:19:24 +0300

    Basic flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route("/", method=['GET'])
+@app.route("/", methods=["GET"])
 def hello():
     return jsonify({"message": "Bienvenue"})
 
