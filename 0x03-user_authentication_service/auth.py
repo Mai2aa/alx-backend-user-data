15c89721a5c6857bde75b89943694abb27031e35
Mai2aa <mysaadil0@gmail.com>
2024-09-12 23:38:19 +0300

    update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,7 +83,7 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
             user = None
-        if user == None:
+        if user is None:
             raise ValueError()
         new_password_hash = _hash_password(password)
         self._db.update_user(
