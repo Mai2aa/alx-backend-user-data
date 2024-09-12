ff3c7543fcab6f18da641a5cd4bd67c9ebfe0f25
Mai2aa <mysaadil0@gmail.com>
2024-09-12 22:49:09 +0300

    update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,7 +79,7 @@ class Auth:
         '''a method that updates the password'''
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-        except NoResultFound:
+        except ValueError:
             user = None
         if user is None:
             raise ValueError("User not found")
